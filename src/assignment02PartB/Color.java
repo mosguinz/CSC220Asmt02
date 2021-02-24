ada017b438aa12c58e447d13c20061d6d148819d
mosguinz <mos.guinz@gmail.com>
2021-02-24 14:05:31 -0800

    Add basic fields and constructors for Color

--- src/assignment02PartB/Color.java
+++ src/assignment02PartB/Color.java
@@ -7,7 +7,6 @@
  * @author: Kullathon Sitthisarnwattanachai
  * **********************************************
  */
-
 package assignment02PartB;
 // Please organize all the given files in 1 same package
 // Please make sure to read the provided "_ListOf-PleaseDoNotChange.txt"
@@ -25,5 +24,25 @@ public class Color {
     //
     // Instance Data Fields
     //
-}
+    /**
+     * The character encoding system.
+     */
+    public final String encoding;
 
+    /**
+     * Initialize {@link Color} with {@link #defaultPreference} as the character
+     * encoding.
+     */
+    public Color() {
+        this.encoding = defaultPreference;
+    }
+
+    /**
+     * Initialize {@link Color} with {@code encoding} as the character encoding.
+     *
+     * @param encoding The character encoding type to use.
+     */
+    public Color(String encoding) {
+        this.encoding = encoding;
+    }
+}
