eae4bd36a5639450321132333e9a1b1e08b9d6ba
mosguinz <mos.guinz@gmail.com>
2021-02-24 13:17:24 -0800

    Add default fields and constructors for Language

--- src/assignment02PartB/Language.java
+++ src/assignment02PartB/Language.java
@@ -7,12 +7,12 @@
  * @author: Kullathon Sitthisarnwattanachai
  * **********************************************
  */
-
 package assignment02PartB;
 // Please organize all the given files in 1 same package
 // Please make sure to read the provided "_ListOf-PleaseDoNotChange.txt"
 
 public class Language {
+
     //
     // Static Data Fields
     //
@@ -20,4 +20,26 @@ public class Language {
     private static final String officialGreeting = "Say Hey Willie";
     private static final String officialSong = "Take Me out to the Ball Game";
     private static final String officialAppName = "SF Giants Thank You";
+
+    /**
+     * The language to use for this instance.
+     */
+    private final String language;
+
+    /**
+     * Initialize {@link Language} with {@link #defaultPreference} as the
+     * language.
+     */
+    public Language() {
+        this.language = defaultPreference;
+    }
+
+    /**
+     * Initialize {@link Language} with {@code language}.
+     *
+     * @param language The language to use.
+     */
+    public Language(String language) {
+        this.language = language;
+    }
 }
