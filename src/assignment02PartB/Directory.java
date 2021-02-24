446367cdc7d36a924324685abb441d262ac559fd
mosguinz <mos.guinz@gmail.com>
2021-02-24 14:34:51 -0800

    Add basic fields and constructors for Directory

--- src/assignment02PartB/Directory.java
+++ src/assignment02PartB/Directory.java
@@ -7,22 +7,38 @@
  * Author: Kullathon Sitthisarnwattanachai
  * **********************************************
  */
-
 package assignment02PartB;
 // Please organize all the given files in 1 same package
 // Please make sure to read the provided "_ListOf-PleaseDoNotChange.txt"
+
+import java.io.File;
 
 public class Directory {
 
     //
     // Instance Data Fields
     //
+    /**
+     * The string representation of the path.
+     */
     private String path;
+    /**
+     * The {@link File} object that represents the directory.
+     */
     private File directory;
 
     //
     // Constructors
     //
     public Directory() {
+    }
+
+    public Directory(String path) {
+        this.path = path;
+        this.directory = createPath(path);
+    }
+
+    private static File createPath(String path) {
+        return;
     }
 }
