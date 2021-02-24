66001dfad7e5c327c2f612c29db5ced41d016633
mosguinz <mos.guinz@gmail.com>
2021-02-24 13:38:00 -0800

    Add basic fields and constructor for Timer

--- src/assignment02PartB/Timer.java
+++ src/assignment02PartB/Timer.java
@@ -7,10 +7,29 @@
  * @author: Kullathon Sitthisarnwattanachai
  * **********************************************
  */
- 
 package assignment02PartB;
 // Please organize all the given files in 1 same package
 // Please make sure to read the provided "_ListOf-PleaseDoNotChange.txt"
 
 public class Timer {
+
+    private static final String defaultTimeZone = "Pacific Standard Time";
+
+    private final String timeZone;
+
+    /**
+     * Initialize {@link Timer} with {@link #defaultTimeZone} as the time zone.
+     */
+    public Timer() {
+        this.timeZone = defaultTimeZone;
+    }
+
+    /**
+     * Initialize {@link Timer} with the {@code timeZone}.
+     *
+     * @param timeZone The time zone to use.
+     */
+    public Timer(String timeZone) {
+        this.timeZone = timeZone;
+    }
 }
