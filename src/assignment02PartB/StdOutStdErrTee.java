65bea03d96bd0240590996df6c9c080d8639186e
mosguinz <mos.guinz@gmail.com>
2021-02-25 00:38:08 -0800

    Add fallback method for creating file streams
     - The fallback will attempt to create streams at the default location
       as defined in Config.
    
    Update Javadoc for createStreams to reflect the change above

--- src/assignment02PartB/StdOutStdErrTee.java
+++ src/assignment02PartB/StdOutStdErrTee.java
@@ -14,6 +14,7 @@ package assignment02PartB;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.io.UncheckedIOException;
 
 public class StdOutStdErrTee extends OutputStream {
 
@@ -26,12 +27,19 @@ public class StdOutStdErrTee extends OutputStream {
 
     /**
      * Returns an array of {@link OutputStream}s to write the output to.
+     * <p>
+     * This method will attempt to create file output streams to write the standard output streams
+     * to with the given filenames.
+     * <p>
+     * If the file exists but is a directory rather than a regular file, does not exist but cannot
+     * be created, or cannot be opened for any other reason then the stream outputs will be stored
+     * under {@link Config#defaultStdOutFilePath} and {@link Config#defaultStdErrFilePath},
+     * respectively.
      *
-     * @param sOutPath The filename under which to write the
-     *                 {@link java.lang.System#out} stream to.
-     * @param sErrPath The filename under which to write the
-     *                 {@link java.lang.System#err} stream to.
+     * @param sOutPath The filename under which to write the {@link java.lang.System#out} stream to.
+     * @param sErrPath The filename under which to write the {@link java.lang.System#err} stream to.
      * @return The array of {@link OutputStream}s.
+     * @see #defaultStreams()
      */
     private OutputStream[] createStreams(String sOutPath, String sErrPath) {
         try {
@@ -39,7 +47,38 @@ public class StdOutStdErrTee extends OutputStream {
                 new FileOutputStream(sOutPath), new FileOutputStream(sErrPath)
             };
         } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
+            System.err.printf(
+                    "Could not create file output streams at '{}' and '{}'. Reason: {}\n"
+                    + "Creating file output streams at default location instead...",
+                    sOutPath, sErrPath, e.getMessage()
+            );
+            return defaultStreams();
+        }
+    }
+
+    /**
+     * Helper of {@link #createStreams(java.lang.String, java.lang.String)} to create default output
+     * streams.
+     * <p>
+     * Due to the requirements defined in the the assessment specification, the default file paths
+     * defined in {@link Config} cannot be edited. As such, exceptions arising from the creation of
+     * the fallback {@link OutputStream}s are treated as "unrecoverable," raising an unchecked
+     * exception as a result.
+     *
+     * @return The array of {@link OutputStream}s.
+     * @throws UncheckedIOException If an I/O error occurs; see above for more information.
+     * @see Config#defaultStdOutFilePath
+     * @see Config#defaultStdErrFilePath
+     * @see #createStreams(java.lang.String, java.lang.String)
+     */
+    private static OutputStream[] defaultStreams() throws UncheckedIOException {
+        try {
+            return new OutputStream[]{
+                new FileOutputStream(Config.getDefaultStdOutFilePath()),
+                new FileOutputStream(Config.getDefaultStdErrFilePath())
+            };
+        } catch (FileNotFoundException e) {
+            throw new UncheckedIOException(e);
         }
     }
 }
