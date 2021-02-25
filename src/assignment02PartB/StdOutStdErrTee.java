aad10385d7e633c4ff17fa718c6f08ecd11d5185
mosguinz <mos.guinz@gmail.com>
2021-02-24 20:40:05 -0800

    Add method to create output streams for OutputStream

--- src/assignment02PartB/StdOutStdErrTee.java
+++ src/assignment02PartB/StdOutStdErrTee.java
@@ -11,18 +11,35 @@ package assignment02PartB;
 // Please organize all the given files in 1 same package
 // Please make sure to read the provided "_ListOf-PleaseDoNotChange.txt"
 
-import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.OutputStream;
 
 public class StdOutStdErrTee extends OutputStream {
 
-    private final String stdOutFilePath;
-    private final String stdErrFilePath;
-    private final File stdOutFile;
-    private final File stdErrFile;
+    private final OutputStream[] OUTPUT_STREAMS;
 
-    public StdOutStdErrTee(String stdOutFilePath, String stdErrFilePath) {
-        this.stdOutFilePath = stdOutFilePath;
-        this.stdErrFilePath = stdErrFilePath;
+    public StdOutStdErrTee(String sOutPath, String sErrPath) {
+        createStreams(sOutPath, sErrPath);
+        OUTPUT_STREAMS = createStreams(sOutPath, sErrPath);
+    }
+
+    /**
+     * Returns an array of {@link OutputStream}s to write the output to.
+     *
+     * @param sOutPath The filename under which to write the
+     *                 {@link java.lang.System#out} stream to.
+     * @param sErrPath The filename under which to write the
+     *                 {@link java.lang.System#err} stream to.
+     * @return The array of {@link OutputStream}s.
+     */
+    private OutputStream[] createStreams(String sOutPath, String sErrPath) {
+        try {
+            return new OutputStream[]{
+                new FileOutputStream(sOutPath), new FileOutputStream(sErrPath)
+            };
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
