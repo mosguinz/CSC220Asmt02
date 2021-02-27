024abeefbc7be6cf4343d24cfec79bcc97e2d6bd
mosguinz <mos.guinz@gmail.com>
2021-02-26 18:00:46 -0800

    Add method to crete Locale object for i18n support

--- src/assignment02PartB/Language.java
+++ src/assignment02PartB/Language.java
@@ -8,9 +8,12 @@
  * **********************************************
  */
 package assignment02PartB;
-// Please organize all the given files in 1 same package
-// Please make sure to read the provided "_ListOf-PleaseDoNotChange.txt"
 
+// Please organize all the given files in 1 same package
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+// Please make sure to read the provided "_ListOf-PleaseDoNotChange.txt"
 public class Language {
 
     //
@@ -21,14 +24,19 @@ public class Language {
     private static final String officialSong = "Take Me out to the Ball Game";
     private static final String officialAppName = "SF Giants Thank You";
 
+    private static final String LANGUAGE_PATH = "assignment02PartB.resources.resources.i18n";
+    private static final Locale DEFAULT_LOCALE = new Locale("en");
+    private static final Locale[] SUPPORTED_LOCALES = {
+        DEFAULT_LOCALE, new Locale("th"), new Locale("alien")
+    };
+
     /**
      * The language to use for this instance.
      */
     private final String language;
 
     /**
-     * Initialize {@link Language} with {@link #defaultPreference} as the
-     * language.
+     * Initialize {@link Language} with {@link #defaultPreference} as the language.
      */
     public Language() {
         this.language = defaultPreference;
@@ -41,5 +49,29 @@ public class Language {
      */
     public Language(String language) {
         this.language = language;
+    }
+
+    public ResourceBundle getBundle(String className) {
+        return ResourceBundle.getBundle(className);
+    }
+
+    /**
+     * Gets a supported language from the input.
+     * <p>
+     * {@code input} can be either the language code or the name of the language itself. Returns
+     * {@link #DEFAULT_LOCALE} if the language is not supported.
+     *
+     * @param input The desired language.
+     * @return The {@link Locale} object representing the language.
+     */
+    private static Locale findLocale(String input) {
+        input = input.strip();
+        for (Locale locale : SUPPORTED_LOCALES) {
+            if (locale.getLanguage().equalsIgnoreCase(input)
+                    || locale.getDisplayLanguage().equalsIgnoreCase(input)) {
+                return locale;
+            }
+        }
+        return DEFAULT_LOCALE;
     }
 }
