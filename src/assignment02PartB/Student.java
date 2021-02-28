378bc6fb991006c48541a7ff2306d10f15950c4e
mosguinz <mos.guinz@gmail.com>
2021-02-27 20:26:52 -0800

    Add basic fields, getter, setters, and constructors for Student

--- src/assignment02PartB/Student.java
+++ src/assignment02PartB/Student.java
@@ -18,4 +18,32 @@ public class Student extends Person {
     // Instance Data Fields
     //
     private University university;
+    private String email;
+
+    public Student(String firstName, String lastName, University university, String email) {
+        super(firstName, lastName);
+        this.university = university;
+        this.email = email;
+    }
+
+    public Student(University university, String email) {
+        this.university = university;
+        this.email = email;
+    }
+
+    public University getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(University university) {
+        this.university = university;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
