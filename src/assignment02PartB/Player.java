c19657a2a53dc7e179292eb88675844af29b3e8b
mosguinz <mos.guinz@gmail.com>
2021-02-28 21:17:00 -0800

    Add constructor for Player to load from RBundle

--- src/assignment02PartB/Player.java
+++ src/assignment02PartB/Player.java
@@ -7,18 +7,57 @@
  * Author: Kullathon Sitthisarnwattanachai
  * **********************************************
  */
-
 package assignment02PartB;
 // Please organize all the given files in 1 same package
 // Please make sure to read the provided "_ListOf-PleaseDoNotChange.txt"
 
+import java.util.ResourceBundle;
+
 public class Player extends Person {
 
-    private String position;
-    private String number;
-    private String batSide;
-    private String throwSide;
-    private String mlbDebut;
+    private final Club club;
+    private final String position;
+    private final String number;
+    private final String batSide;
+    private final String throwSide;
+    private final String mlbDebut;
+
+    public Player(Club club) {
+        super();
+        final ResourceBundle bundle = ResourceBundle
+                .getBundle("assignment02PartB.resources.PlayerInfo");
+        this.setFirstName(bundle.getString("firstName"));
+        this.setLastName(bundle.getString("lastName"));
+        this.club = club;
+        position = bundle.getString("position");
+        number = bundle.getString("number");
+        batSide = bundle.getString("batSide");
+        throwSide = bundle.getString("throwSide");
+        mlbDebut = bundle.getString("mlbDebut");
+    }
+
+    public void displayInfo() {
+        final ResourceBundle bundle = Messenger.getConfig().getLang().getBundle("Player");
+        String[] fields = {
+                bundle.getString("player.label"),
+                bundle.getString("club.label"),
+                bundle.getString("position.label"),
+                bundle.getString("number.label"),
+                bundle.getString("batSide.label"),
+                bundle.getString("throwSide.label"),
+                bundle.getString("mlbDebut.label")
+        };
+        String[] values = {
+                super.toString(),
+                club.getOfficialName(),
+                position,
+                number,
+                batSide,
+                throwSide,
+                mlbDebut
+        };
+        ChatSession.printTable(fields, values);
+    }
 
     @Override
     public String toString() {
