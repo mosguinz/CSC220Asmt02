de79f23aea6077eeff40e77aaf2828e2141af8bf
mosguinz <mos.guinz@gmail.com>
2021-03-01 13:25:48 -0800

    Add method to load questions and answers

--- src/assignment02PartB/Quiz.java
+++ src/assignment02PartB/Quiz.java
@@ -13,6 +13,7 @@ package assignment02PartB;
 // Please make sure to read the provided "_ListOf-PleaseDoNotChange.txt"
 
 import java.util.ArrayList;
+import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Quiz {
@@ -22,12 +23,37 @@ public class Quiz {
     private final Club club;
     private final Student student;
     private final ResourceBundle bundle;
-    private ArrayList<QuestionAnswer> questionAnswer;
+    private final ArrayList<QuestionAnswer> questionAnswer;
 
     public Quiz(Club club, Student student) {
         bundle = Messenger.getConfig().getLang().getBundle("Quiz");
         this.club = club;
         this.student = student;
+        this.questionAnswer = loadQuestionAnswers();
     }
 
+    /**
+     * Loads the questions and answers from the resource bundle.
+     * <p>
+     * Stops when the question and answer key-pairs in the resource bundle have been fully
+     * enumerated.
+     *
+     * @return The {@link ArrayList} of {@link QuestionAnswer}.
+     */
+    private ArrayList<QuestionAnswer> loadQuestionAnswers() {
+        int i = 1;
+        ArrayList<QuestionAnswer> questionAnswer = new ArrayList<>();
+        while (true) {
+            try {
+                questionAnswer.add(
+                        new QuestionAnswer(
+                                bundle.getString("question." + i),
+                                bundle.getString("answer." + i)
+                        ));
+                i++;
+            } catch (MissingResourceException e) {
+                return questionAnswer;
+            }
+        }
+    }
 }
