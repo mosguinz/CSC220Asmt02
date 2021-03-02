19ff25493c35f460cefc4e8b6eb202fc51ff4a7b
mosguinz <mos.guinz@gmail.com>
2021-03-01 21:46:50 -0800

    Add RBundle file for university

--- src/assignment02PartB/University.java
+++ src/assignment02PartB/University.java
@@ -12,14 +12,20 @@ package assignment02PartB;
 // Please organize all the given files in 1 same package
 // Please make sure to read the provided "_ListOf-PleaseDoNotChange.txt"
 
+import java.util.ResourceBundle;
+
 public class University {
 
-    private static final String DEFAULT_NAME = "San Francisco State University";
-
+    /**
+     * Name of the university. Note that this is independent of the default university name defined
+     * in Config and is never localized.
+     */
     private final String name;
 
     public University() {
-        this.name = DEFAULT_NAME;
+        final ResourceBundle bundle = ResourceBundle
+                .getBundle("assignment02PartB.resources.UniversityData");
+        this.name = bundle.getString("name");
     }
 
     public String getName() {
