186bec0cae91c14697c4631095c89c652b3e63cc
mosguinz <mos.guinz@gmail.com>
2021-03-01 22:56:20 -0800

    Add method to set card and quiz for Receipt

--- src/assignment02PartB/Receipt.java
+++ src/assignment02PartB/Receipt.java
@@ -18,16 +18,28 @@ public class Receipt {
 
     private static final ResourceBundle bundle = Messenger.getConfig().getLang()
             .getBundle("Receipt");
+    private static final String path = Messenger.getConfig().getReceiptFilePath();
+
     private final String startTimestamp;
     private final Student student;
     private final Player player;
     private String endTimestamp;
     private Card card;
+    private Quiz quiz;
 
     public Receipt(Student student, Player player) {
         startTimestamp = ChatSession.generateTimestamp("tx.start");
         this.student = student;
         this.player = player;
     }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public void setQuiz(Quiz quiz) {
+        this.quiz = quiz;
+    }
+
 
 }
