865955d0a5342f8c90e794c393a0cf9adbaade5c
mosguinz <mos.guinz@gmail.com>
2021-03-01 22:43:41 -0800

    Add basic fields and constructors for receipt class

--- src/assignment02PartB/Receipt.java
+++ src/assignment02PartB/Receipt.java
@@ -12,5 +12,22 @@ package assignment02PartB;
 // Please organize all the given files in 1 same package
 // Please make sure to read the provided "_ListOf-PleaseDoNotChange.txt"
 
+import java.util.ResourceBundle;
+
 public class Receipt {
+
+    private static final ResourceBundle bundle = Messenger.getConfig().getLang()
+            .getBundle("Receipt");
+    private final String startTimestamp;
+    private final Student student;
+    private final Player player;
+    private String endTimestamp;
+    private Card card;
+
+    public Receipt(Student student, Player player) {
+        startTimestamp = ChatSession.generateTimestamp("tx.start");
+        this.student = student;
+        this.player = player;
+    }
+
 }
