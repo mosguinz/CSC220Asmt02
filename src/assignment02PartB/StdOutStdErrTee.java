0253cbde82c70dc794cae4722d7c312c4159fba5
mosguinz <mos.guinz@gmail.com>
2021-03-01 17:49:12 -0800

    Fix logging tee

--- src/assignment02PartB/StdOutStdErrTee.java
+++ src/assignment02PartB/StdOutStdErrTee.java
@@ -21,35 +21,40 @@ import java.io.UncheckedIOException;
 public class StdOutStdErrTee extends OutputStream {
 
     private final OutputStream[] OUTPUT_STREAMS;
+    private String sOutPath;
+    private String sErrPath;
 
     public StdOutStdErrTee(String sOutPath, String sErrPath) {
-        createStreams(sOutPath, sErrPath);
-        OUTPUT_STREAMS = createStreams(sOutPath, sErrPath);
         this.sOutPath = sOutPath;
         this.sErrPath = sErrPath;
+        createFileStreams(sOutPath, sErrPath);
+        OUTPUT_STREAMS = createFileStreams(sOutPath, sErrPath);
+    }
+
+    private StdOutStdErrTee(PrintStream printStream, FileOutputStream fOutStream) {
+        OUTPUT_STREAMS = new OutputStream[]{printStream, fOutStream};
     }
 
     /**
-     * Returns an array of {@link OutputStream}s to write the output to.
+     * Returns an array of {@link FileOutputStream}s to write the output to.
      * <p>
      * This method will attempt to create file output streams to write the standard output streams
      * to with the given filenames.
      * <p>
      * If the file exists but is a directory rather than a regular file, does not exist but cannot
-     * be created, or cannot be opened for any other reason then the stream outputs will be stored
-     * under {@link Config#getDefaultStdOutFilePath()} and {@link Config#getDefaultStdErrFilePath()},
-     * respectively.
+     * be created, or cannot be opened for any other reason then the method will attempt to create
+     * the output streams at the default location instead.
      *
      * @param sOutPath The filename under which to write the {@link java.lang.System#out} stream
      *                 to.
      * @param sErrPath The filename under which to write the {@link java.lang.System#err} stream
      *                 to.
-     * @return The array of {@link OutputStream}s.
-     * @see #defaultStreams()
+     * @return The array of {@link FileOutputStream}s.
+     * @see #defaultFileStreams()
      */
-    private static OutputStream[] createStreams(String sOutPath, String sErrPath) {
+    private static FileOutputStream[] createFileStreams(String sOutPath, String sErrPath) {
         try {
-            return new OutputStream[]{
+            return new FileOutputStream[]{
                     new FileOutputStream(sOutPath), new FileOutputStream(sErrPath)
             };
         } catch (FileNotFoundException e) {
@@ -58,28 +63,28 @@ public class StdOutStdErrTee extends OutputStream {
                             + "Creating file output streams at default location instead...",
                     sOutPath, sErrPath, e.getMessage()
             );
-            return defaultStreams();
+            return defaultFileStreams();
         }
     }
 
     /**
-     * Helper of {@link #createStreams(java.lang.String, java.lang.String)} to create default output
-     * streams.
+     * Helper of {@link #createFileStreams(java.lang.String, java.lang.String)} to create output
+     * streams at the default location.
      * <p>
      * Due to the requirements defined in the the assessment specification, the default file paths
      * defined in {@link Config} cannot be edited. As such, exceptions arising from the creation of
-     * the fallback {@link OutputStream}s are treated as "unrecoverable," raising an unchecked
-     * exception as a result.
+     * the fallback {@link FileOutputStream}s are treated as "unrecoverable" and will raise an
+     * unchecked exception as a result.
      *
-     * @return The array of {@link OutputStream}s.
+     * @return The array of {@link FileOutputStream}s.
      * @throws UncheckedIOException If an I/O error occurs; see above for more information.
      * @see Config#getDefaultStdOutFilePath()
      * @see Config#getDefaultStdErrFilePath()
-     * @see #createStreams(java.lang.String, java.lang.String)
+     * @see #createFileStreams(java.lang.String, java.lang.String)
      */
-    private static OutputStream[] defaultStreams() throws UncheckedIOException {
+    private static FileOutputStream[] defaultFileStreams() throws UncheckedIOException {
         try {
-            return new OutputStream[]{
+            return new FileOutputStream[]{
                     new FileOutputStream(Config.getDefaultStdOutFilePath()),
                     new FileOutputStream(Config.getDefaultStdErrFilePath())
             };
@@ -96,11 +101,17 @@ public class StdOutStdErrTee extends OutputStream {
     }
 
     /**
-     * Sets the {@link PrintStream}s to start logging.
+     * Starts logging.
      */
     public void startLog() {
-        System.setOut(new PrintStream(OUTPUT_STREAMS[0]));
-        System.setErr(new PrintStream(OUTPUT_STREAMS[1]));
+        FileOutputStream[] streams = createFileStreams(sOutPath, sErrPath);
+        StdOutStdErrTee sOutTee = new StdOutStdErrTee(System.out, streams[0]);
+        StdOutStdErrTee sErrTee = new StdOutStdErrTee(System.err, streams[1]);
+        PrintStream sOut = new PrintStream(sOutTee);
+        PrintStream sErr = new PrintStream(sErrTee);
+
+        System.setOut(sOut);
+        System.setErr(sErr);
     }
 
     @Override
