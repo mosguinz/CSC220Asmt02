e01441afc97421656eab251a901e36d546b46b15
mosguinz <mos.guinz@gmail.com>
2021-04-01 19:20:44 -0700

    Update Javadoc

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"assignment02PartB"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"assignment02PartB"}];updateSearchResults();
